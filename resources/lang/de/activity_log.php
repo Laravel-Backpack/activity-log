bda986ef3b4550b20141ddd08cdd255ce6791ea6
Frank Stuhec <info@secondreality.com>
2023-04-04 11:51:10 +0200

    Add german translation

---
+++ resources/lang/de/activity_log.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'activity_log' => 'Aktivitätsprotokoll',
+    'activity_logs' => 'Aktivitätsprotokolle',
+
+    'causer' => 'Verursacher',
+    'subject' => 'Ziel',
+    'causer_model' => 'verursachendes Model',
+    'subject_model' => 'Ziel Model',
+    'event' => 'Ereignis',
+    'date' => 'Zeitpunkt',
+    'changes' => 'Änderungen',
+    'key' => 'Primärschlüssel',
+    'previous_value' => 'vorherige Werte',
+    'new_value' => 'Neue Werte',
+
+    'created' => 'erstellt',
+    'updated' => 'geändert',
+    'deleted' => 'gelöscht',
+    'restored' => 'weiderhergestellt',
+];
