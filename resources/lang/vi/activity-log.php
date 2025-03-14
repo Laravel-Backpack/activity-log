6fa1eb360a203e2b6e53e2149dfbaf1994c5a056
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2025-03-15 00:31:54 +0700

    add lang vietnamese

---
+++ resources/lang/vi/activity-log.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'activity_log' => 'nhật ký hoạt động',
+    'activity_logs' => 'các nhật ký hoạt động',
+
+    'activity_log_button_causer' => 'hoạt động được thực hiện',
+    'activity_log_button_subject' => 'hoạt động',
+
+    'causer' => 'người thực hiện',
+    'subject' => 'đối tượng',
+    'causer_model' => 'loại người thực hiện',
+    'subject_model' => 'loại đối tượng',
+    'event' => 'sự kiện',
+    'date' => 'ngày',
+    'changes' => 'các thay đổi',
+    'key' => 'khóa',
+    'previous_value' => 'giá trị trước đó',
+    'new_value' => 'giá trị mới',
+
+    'created' => 'đã tạo',
+    'updated' => 'đã cập nhật',
+    'deleted' => 'đã xóa',
+    'restored' => 'đã khôi phục',
+];
